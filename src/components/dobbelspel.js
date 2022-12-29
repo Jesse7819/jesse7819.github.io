d82121682bb242bed05580df31db03e1df2481d6
Jesse7819 <jessesosef.js@gmail.com>
2022-12-29 09:47:00 +0100

    dice game

--- src/components/dobbelspel.js
+++ src/components/dobbelspel.js
@@ -7,6 +7,7 @@ import dice2 from '../images/dice2.png';
 import dice1 from '../images/dice1.png';
 
 var diceRollInfo = false;
+
 var player1DiceRoll1;
 var player1DiceRoll2;
 var player2DiceRoll1;
@@ -18,127 +19,122 @@ var beurt;
 var player1Name = "";
 var player2Name = "";
 var winner;
-var snakeEyes1 = false;
-var snakeEyes2 = false;
 let diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
-
-
-
 
 export function diceRollInfoFunction() {
 
     player1Name = prompt("Wat is de naam van speler 1?");
     player2Name = prompt("Wat is de naam van speler 2?");
-    
-    if(diceRollInfo === false) {
-        if ((player1Name && player2Name) !== "" || null) {
-            // document.getElementsByClassName("player1Name")[0].innerHTML = "Speler: " + player1Name;
-            // document.getElementsByClassName("player2Name")[0].innerHTML = "Speler: " + player2Name;
-            $(".player1Name").text("Speler: " + player1Name);
-            $(".player2Name").text("Speler: " + player2Name);
-            diceRollInfo = true;
-            $(".resetBtn").css("display", "block");
-            $(".rollDice").css("display", "block");
-            $(".startBtn").css("display", "none");
-            getTurnPlayer();
+
+    if (diceRollInfo === false) {
+        if (((player1Name && player2Name) !== null)) {
+            if (((player1Name && player2Name) !== "")) {
+                $(".player1Name").text("Player 1: " + player1Name);
+                $(".player2Name").text("Player 2: " + player2Name);
+                diceRollInfo = true;
+                $(".resetBtn").css("display", "block");
+                $(".rollDice").css("display", "block");
+                $(".startBtn").css("display", "none");
+                $(".rightArea").css("opacity", "0.5");
+            } else {
+                $(".player1Name").text("Oh, U heeft niets ingevuld! Wilt u uw naam niet vertellen?");
+                $(".player2Name").text("");
+                diceRollInfo = false;
+                $(".resetBtn").css("display", "none");
+                $(".rollDice").css("display", "none");
+            }
         } else {
-            // document.getElementsByClassName("player1Name")[0].innerHTML = "Oh, U heeft niets ingevuld! Wilt u uw naam niet vertellen?";
-            // document.getElementsByClassName("player2Name")[0].innerHTML = "";
             $(".player1Name").text("Oh, U heeft niets ingevuld! Wilt u uw naam niet vertellen?");
             $(".player2Name").text("");
             diceRollInfo = false;
             $(".resetBtn").css("display", "none");
-            $(".rollDiceBtn").css("display", "none");
+            $(".rollDice").css("display", "none");
         }
     }
-
 }
 
-export function getTurnPlayer() {
+export function resetDiceRollFunction() {
+    console.log(player1Name);
+    diceRollInfo = false;
+    // player1Name = "";
+    // player2Name = "";
+    $(".player1Name").text("Player 1: ");
+    $(".player2Name").text("Player 2: ");
+    player1DiceRoll1 = 0;
+    player1DiceRoll2 = 0;
+    player2DiceRoll1 = 0;
+    player2DiceRoll2 = 0;
+    // $(".resetBtn").css("display", "none");
+    $(".rollDice").css("display", "none");
+    $(".startBtn").css("display", "block");
+    $(".dices1").css("display", "none");
+    $(".dices2").css("display", "none");
+    turn = "player1";
+    $(".playerTurn").css("display", "none");
+    $(".resultaat").text("");
+    $(".rightArea").css("opacity", "1");
+    $(".leftArea").css("opacity", "1");
+}
 
-    if(turn === "player1"){
+function rollDice() {
+    return Math.floor(Math.random() * 6 + 1);
+}
+
+export function startGameFunction() {
+
+    if (turn === "player1") {
+        player1DiceRoll1 = rollDice();
+        player1DiceRoll2 = rollDice();
+        player1Total = (player1DiceRoll1 + player1DiceRoll2);
+        $(".dices1").css("display", "block");
+        document.querySelector(".diceOne").setAttribute("src", diceImages[player1DiceRoll1 - 1]);
+        document.querySelector(".diceTwo").setAttribute("src", diceImages[player1DiceRoll2 - 1]);
+        turn = "player2";
+        $(".player1Name").text("Player: " + player1Name + ": heeft " + player1DiceRoll1 + " + " + player1DiceRoll2 + " (" + player1Total + ") gerold.");
+    } else if (turn === "player2") {
+        player2DiceRoll1 = rollDice();
+        player2DiceRoll2 = rollDice();
+        player2Total = (player2DiceRoll1 + player2DiceRoll2);
+        $(".dices2").css("display", "block");
+        document.querySelector(".diceThree").setAttribute("src", diceImages[player2DiceRoll1 - 1]);
+        document.querySelector(".diceFour").setAttribute("src", diceImages[player2DiceRoll2 - 1]);
+        turn = "player1";
+        $(".player2Name").text("Player: " + player2Name + ": heeft " + player2DiceRoll1 + " + " + player2DiceRoll2 + " (" + player2Total + ") gerold.");
+        getResultaat();
+    }
+    if (turn === "player1") {
         beurt = player1Name + " is aan de beurt.";
+        $(".rightArea").css("opacity", "0.5");
+        $(".leftArea").css("opacity", "1");
     } else {
         beurt = player2Name + " is aan de beurt.";
+        $(".rightArea").css("opacity", "1");
+        $(".leftArea").css("opacity", "0.5");
     }
     $(".playerTurn").css("display", "block");
     $(".playerTurn").text(beurt);
 }
 
-export function resetDiceRollFunction() {
-    diceRollInfo = false;
-    player1Name = "";
-    player2Name = "";
-    $(".player1Name").text("");
-    $(".player2Name").text("");
-    player1DiceRoll1 = 0;
-    player1DiceRoll2 = 0;
-    player2DiceRoll1 = 0;
-    player2DiceRoll2 = 0;
-    $(".resetBtn").css("display", "none");
-    $(".rollDiceBtn").css("display", "none");
-    $(".startBtn").css("display", "block");
-    $(".diceOne").text("");
-    $(".diceTwo").text("");
-    turn = "player1";
-    $(".playerTurn").css("display", "none");
-    snakeEyes1 = false;
-    snakeEyes2 = false;
-}
-
-export function startGameFunction() {
-
-
-    if(turn === "player1") {
-        
-        player1DiceRoll1 = Math.floor(Math.random() * 6 + 1);
-        player1DiceRoll2 = Math.floor(Math.random() * 6 + 1);
-        
-        player1Total = (player1DiceRoll1 + player1DiceRoll2);
-        document.querySelector(".diceOne").setAttribute("src", diceImages[player1DiceRoll1 - 1]);
-        document.querySelector(".diceTwo").setAttribute("src", diceImages[player1DiceRoll2 - 1]);
-        if(player1Total === 2) {
-            snakeEyes1 = true;
-        }
-        turn = "player2";
-        getTurnPlayer();
-        $(".player1Name").text("Speler: " + player1Name + ": heeft " + player1DiceRoll1 + " + " + player1DiceRoll2 + " (" + player1Total + ") gerold.");
-    } else if(turn === "player2") {
-        player2DiceRoll1 = Math.floor(Math.random() * 6 + 1);
-        player2DiceRoll2 = Math.floor(Math.random() * 6 + 1);
-        player2Total = (player2DiceRoll1 + player2DiceRoll2);
-        document.querySelector(".diceOne").setAttribute("src", diceImages[player2DiceRoll1 - 1]);
-        document.querySelector(".diceTwo").setAttribute("src", diceImages[player2DiceRoll2 - 1]);
-        if(player2Total === 2) {
-            snakeEyes2 = true;
-        }
-        turn = "player1";
-        getTurnPlayer();
-        $(".player2Name").text("Speler: " + player2Name + ": heeft " + player2DiceRoll1 + " + " + player2DiceRoll2 + " (" + player2Total + ") gerold.");
-        getResultaat();
-    }
-
-}
-
 export function getResultaat() {
-
-    if(snakeEyes1 === true || snakeEyes2 === true) {
-        if(snakeEyes1 === true) {
-            winner = player1Name;
-        } 
-        if(snakeEyes2 === true) {
-            winner = player2Name;
-        }
-    } else if((snakeEyes1 === true && snakeEyes2 === true) || (player1Total === player2Total)) {
+    if (player1Total === player2Total) {
         $(".resultaat").text("Gelijkspel!");
         return;
-    } 
-    if(player1Total > player2Total) {
+    }
+    else if (player1Total === 2) {
         winner = player1Name;
-    } 
-    if(player2Total > player1Total) {
+        $(".resultaat").text(player1Name + " heeft gewonnen door snake eyes!");
+        return;
+    }
+    else if (player2Total === 2) {
+        winner = player2Name;
+        $(".resultaat").text(player2Name + " heeft gewonnen door snake eyes!");
+        return;
+    }
+    else if (player1Total > player2Total) {
+        winner = player1Name;
+    }
+    else if (player2Total > player1Total) {
         winner = player2Name;
     }
-
     $(".resultaat").text(winner + " heeft gewonnen!");
 }
--- src/pages/dobbelspel.js
+++ src/pages/dobbelspel.js
@@ -3,28 +3,40 @@ import { diceRollInfoFunction, resetDiceRollFunction, startGameFunction } from "
 import dice6 from '../images/dice6.png';
 const Dobbelspel = () => {
     return (
-        
+
         <div className="dobbelspelContainer">
-            <div className="diceButtons">
+            <h1 className="dobbelspelTitel">Dobbelspel</h1>
+            <div className="areas">
+                <div className="leftArea">
+                    <h2 className="player1Name">Player 1: </h2>
+                    <div className="dices1">
+                        <img className="diceOne" src={dice6} alt="#"></img>
+                        <img className="diceTwo" src={dice6} alt="#"></img>
+                    </div>
+                </div>
+
+                <div className="rightArea">
+                    <h2 className="player2Name">Player 2: </h2>
+                    <div className="dices2">
+                        <img className="diceThree" src={dice6} alt="#"></img>
+                        <img className="diceFour" src={dice6} alt="#"></img>
+                    </div>
+                </div>
+
                 <button className="startBtn" type="button" onClick={diceRollInfoFunction}>Start spel</button>
                 {/* <button class="rollDiceBtn" type="button" onclick="startGameFunction()">Begin het spel</button> */}
-                <button className="resetBtn" type="button" onClick={resetDiceRollFunction}>Stop het spel</button>
+                <button className="resetBtn" type="button" onClick={resetDiceRollFunction}>Stop spel</button>
+                <button className="rollDice" type="button" onClick={startGameFunction}>Rol de stenen</button>
             </div>
-
-            <p className="player1Name"></p>
-            <p className="player2Name"></p>
 
             <div className="playerTurn"></div>
-            <button className="rollDice" type="button" onClick={startGameFunction}>Rol de dobbelstenen</button>
+            
 
-            <div className="numbersRolled">
-                <img className="diceOne" src={dice6} alt="#"></img>
-                <img className="diceTwo" src={dice6} alt="#"></img>
-            </div>
+
             <div className="resultaat"></div>
         </div>
-       
-        
+
+
     );
 };
 
