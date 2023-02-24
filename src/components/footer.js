6e6e9f9cdb94efd10f4b8011bf3061c8214920fd
Jesse Sosef <Jessesosef.js@gmail.com>
2023-02-24 12:14:01 +0100

    footer af

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,85 +1,38 @@
 import React from 'react';
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-  MDBIcon,
-  MDBBtn
-} from 'mdb-react-ui-kit';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGithub, faGoogle, faInstagram, faLinkedin, faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
-  return (
-    <MDBFooter className='bg-light text-center text-white'>
-      <MDBContainer className='p-4 pb-0'>
-        <section className='mb-4'>
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#3b5998' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='facebook-f' />
-          </MDBBtn>
-
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#55acee' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='twitter' />
-          </MDBBtn>
-
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#dd4b39' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='google' />
-          </MDBBtn>
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#ac2bac' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='instagram' />
-          </MDBBtn>
-
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#0082ca' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='linkedin-in' />
-          </MDBBtn>
-
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#333333' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='github' />
-          </MDBBtn>
-        </section>
-      </MDBContainer>
-
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright:
-        <a className='text-white' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
-        </a>
-      </div>
-    </MDBFooter>
-  );
+	return (
+		<div className="footer">
+			<div className="footer-header">
+				<div className="socialBtns">
+					<i className="facebookBtn">
+						<FontAwesomeIcon icon={faFacebook} />
+					</i>
+					<i className="twitterBtn">
+						<FontAwesomeIcon icon={faTwitterSquare} />
+					</i>
+					<i className="googleBtn">
+						<FontAwesomeIcon icon={faGoogle} />
+					</i>
+					<i className="instagramBtn">
+						<FontAwesomeIcon icon={faInstagram} />
+					</i>
+					<i className="linkedInBtn">
+						<FontAwesomeIcon icon={faLinkedin} />
+					</i>
+					<i className="githubBtn">
+						<FontAwesomeIcon icon={faGithub} />
+					</i>
+				</div>
+			</div>
+			<div className="footer-bottom">
+				© 2023 Copyright:
+				<a className="text-white" href='#'>
+					Jesse Sosef
+				</a>
+			</div>
+		</div>
+	);
 }
